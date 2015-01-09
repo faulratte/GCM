ae9f7f1fec5586cc383f957469044385c96cced9
Marcel <marcel.gross@4ebf.de>
2015-01-09 12:07:32 +0100

    Removed PowerManager

--- Client/GCMApp/src/com/gcmexample/firstgcm/GCMIntentService.java
+++ Client/GCMApp/src/com/gcmexample/firstgcm/GCMIntentService.java
@@ -1,15 +1,11 @@
 package com.gcmexample.firstgcm;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.PowerManager;
 import android.util.Log;
 
 import com.google.android.gcm.GCMBaseIntentService;
@@ -63,25 +59,6 @@ public class GCMIntentService extends GCMBaseIntentService {
 
 		NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 		manager.notify(R.string.app_name, notification);
-
-		{
-			// Wake Android Device when notification received
-			PowerManager pm = (PowerManager) context
-					.getSystemService(Context.POWER_SERVICE);
-			final PowerManager.WakeLock mWakelock = pm.newWakeLock(
-					PowerManager.FULL_WAKE_LOCK
-							| PowerManager.ACQUIRE_CAUSES_WAKEUP, "GCM_PUSH");
-			mWakelock.acquire();
-
-			// Timer before putting Android Device to sleep mode.
-			Timer timer = new Timer();
-			TimerTask task = new TimerTask() {
-				public void run() {
-					mWakelock.release();
-				}
-			};
-			timer.schedule(task, 5000);
-		}
 
 	}
 
